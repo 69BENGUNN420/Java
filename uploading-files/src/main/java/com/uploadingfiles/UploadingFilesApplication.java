f405d1e9fcc88cf44b2aa087af09aaee5d337353
Bamgladesh <Bamgladesh>
2020-02-10 19:22:06 +0200

    a

--- uploading-files/src/main/java/com/uploadingfiles/UploadingFilesApplication.java
+++ uploading-files/src/main/java/com/uploadingfiles/UploadingFilesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UploadingFilesApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UploadingFilesApplication.class, args);
 	}
